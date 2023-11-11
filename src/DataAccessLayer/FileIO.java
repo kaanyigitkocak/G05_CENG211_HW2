7700db6934b5947dd9d39f020f4a5d4c0bc13997
dkwns <kaandogan2013@gmail.com>
2023-11-11 18:53:35 +0300

    FileIO class
    FileIO class created.

---
+++ src/DataAccessLayer/FileIO.java
@@ -0,0 +1,56 @@
+package DataAccessLayer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import TestProject.City;
+import TestProject.Country;
+
+public class FileIO {
+	public static final String filePath = "countries_and_cities.csv";
+
+	public static List<Country> readCountries() {
+		
+		ArrayList<Country> countries = new ArrayList<>();
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                Country country = new Country(data[0]);
+                countries.add(country);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+		return countries;
+	}
+	
+	public static List<City> readCities() {
+		
+		ArrayList<City> cities = new ArrayList<>();
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            boolean firstLoop = true;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(", "); 
+                for(String cityName: data) {
+                	if(!firstLoop) {
+                		City city = new City(cityName);
+                		cities.add(city);
+                	} else {
+                		firstLoop = false;
+                	}
+                }
+                firstLoop = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+		return cities;
+	}
+}
